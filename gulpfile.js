dc579ea6805591533d80e0381b4da17335b37285
RafaelBadaro <rafabadaro.15@gmail.com>
2023-03-22 23:26:01 -0300

    dist update

--- gulpfile.js
+++ gulpfile.js
@@ -16,7 +16,7 @@ gulp.task('copy-js', () => {
         'node_modules/bootstrap/dist/js/bootstrap.bundle.min.js',
     ])
         .pipe(newer('./dist/js'))
-        .pipe(notify({message: 'Copy JS files'}))
+        .pipe(notify({ message: 'Copy JS files' }))
         .pipe(gulp.dest('./dist/js'));
 });
 
@@ -42,26 +42,41 @@ gulp.task('sass', () =>
         .pipe(gulp.dest('./dist/css'))
 );
 
+gulp.task('copy-index.html', () => {
+    return gulp.src('index.html')
+        .pipe(newer('./dist/index.html'))
+        .pipe(notify({ message: 'Copy index.html' }))
+        .pipe(gulp.dest('./dist/index.html'));
+});
+
+gulp.task('copy-cases.html', () => {
+    return gulp.src('cases.html')
+        .pipe(newer('./dist/cases.html'))
+        .pipe(notify({ message: 'Copy cases.html' }))
+        .pipe(gulp.dest('./dist/cases.html'));
+});
+
+
+
 gulp.task('browser-sync', () => {
     browserSync.init({
         server: {
             baseDir: './dist',
             directory: true,
         },
-        server: {
-            startPath: 'index.html',
-            port: 7777,
-            ui: {
-                port: 7779,
-            },
-        }
+        startPath: 'index.html',
+        port: 7777,
+        ui: {
+            port: 7779,
+        },
     });
     gulp.watch('./scss/**/*.scss', gulp.series('sass'));
     gulp.watch('index.html').on('change', browserSync.reload);
     gulp.watch('*.html').on('change', browserSync.reload);
     gulp.watch('*/*.js').on('change', browserSync.reload);
     gulp.watch('*/*.css').on('change', browserSync.reload);
+    gulp.watch('./dist/**/*.{html,css,js}').on('change', browserSync.reload);
 });
 
 gulp.task('build', gulp.series('sass'));
-gulp.task('default', gulp.series('copy-js', 'sass', 'browser-sync'));
+gulp.task('default', gulp.series('copy-js', 'sass', 'copy-index.html', 'copy-cases.html', 'browser-sync'));
